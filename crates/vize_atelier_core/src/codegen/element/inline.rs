2f093d316ff6dc359380111a775d43e44c855a93
Ubugeeei <ubuge1122@gmail.com>
2026-06-05 23:46:59 +0900

    test: update svg vnode expectations

--- crates/vize_atelier_core/src/codegen/element/inline.rs
+++ crates/vize_atelier_core/src/codegen/element/inline.rs
@@ -36,6 +36,11 @@ use vize_carton::ToCompactString;
 
 /// Generate element code (non-block)
 pub fn generate_element(ctx: &mut CodegenContext, el: &ElementNode<'_>) {
+    if el.tag_type == ElementType::Element && el.ns != Namespace::Html && el.tag == "svg" {
+        super::block::generate_element_block(ctx, el);
+        return;
+    }
+
     if el.tag_type == ElementType::Component
         && (is_dynamic_component(el)
             || matches!(
