6f3057b2234509d7d1355015d5dfb19ff981e18f
ubugeeei <ubuge1122@gmail.com>
2026-06-13 14:30:04 +0000

    style: apply cargo fmt
    Co-authored-by: Codesmith <codesmith-bot@users.noreply.github.com>

--- crates/vize_patina/src/linter/script_rules/registry/rules.rs
+++ crates/vize_patina/src/linter/script_rules/registry/rules.rs
@@ -9,10 +9,9 @@
 //! addition in `names.rs`.
 
 use super::names::{
-    RULE_NO_ARROW_FUNCTIONS_IN_WATCH, RULE_NO_ASYNC_IN_COMPUTED,
-    RULE_NO_DEEP_DESTRUCTURE_IN_PROPS, RULE_NO_DUPE_KEYS,
-    RULE_NO_GET_CURRENT_INSTANCE, RULE_NO_IMPORT_COMPILER_MACROS, RULE_NO_INTERNAL_IMPORTS,
-    RULE_NO_NEXT_TICK, RULE_NO_OPTIONS_API, RULE_NO_REACTIVE_DESTRUCTURE,
+    RULE_NO_ARROW_FUNCTIONS_IN_WATCH, RULE_NO_ASYNC_IN_COMPUTED, RULE_NO_DEEP_DESTRUCTURE_IN_PROPS,
+    RULE_NO_DUPE_KEYS, RULE_NO_GET_CURRENT_INSTANCE, RULE_NO_IMPORT_COMPILER_MACROS,
+    RULE_NO_INTERNAL_IMPORTS, RULE_NO_NEXT_TICK, RULE_NO_OPTIONS_API, RULE_NO_REACTIVE_DESTRUCTURE,
     RULE_NO_RESERVED_IDENTIFIERS, RULE_NO_SIDE_EFFECTS_IN_COMPUTED,
     RULE_NO_TOP_LEVEL_REF_IN_SCRIPT, RULE_NO_WITH_DEFAULTS, RULE_PINIA_PREFER_STORE_TO_REFS,
     RULE_PREFER_COMPUTED, RULE_PREFER_IMPORT_FROM_VUE, RULE_PREFER_REF_OVER_REACTIVE,
@@ -26,12 +25,12 @@ use super::{
 };
 use crate::rules::script::{
     NoArrowFunctionsInWatch, NoAsyncInComputed, NoDeepDestructureInProps, NoDupeKeys,
-    NoGetCurrentInstance,
-    NoImportCompilerMacros, NoInternalImports, NoNextTick, NoOptionsApi, NoReactiveDestructure,
-    NoReservedIdentifiers, NoSideEffectsInComputed, NoTopLevelRefInScript, NoWithDefaults,
-    PiniaPreferStoreToRefs, PreferComputed, PreferImportFromVue, PreferRefOverReactive,
-    PreferUseAttrs, PreferUseId, PreferUseSlots, PreferUseTemplateRef, RequireFunctionReturnType,
-    RequireSymbolProvide, VueRouterPreferNamedPush, VueTestUtilsNoHtmlSnapshot,
+    NoGetCurrentInstance, NoImportCompilerMacros, NoInternalImports, NoNextTick, NoOptionsApi,
+    NoReactiveDestructure, NoReservedIdentifiers, NoSideEffectsInComputed, NoTopLevelRefInScript,
+    NoWithDefaults, PiniaPreferStoreToRefs, PreferComputed, PreferImportFromVue,
+    PreferRefOverReactive, PreferUseAttrs, PreferUseId, PreferUseSlots, PreferUseTemplateRef,
+    RequireFunctionReturnType, RequireSymbolProvide, VueRouterPreferNamedPush,
+    VueTestUtilsNoHtmlSnapshot,
 };
 
 static NO_DEEP_DESTRUCTURE_IN_PROPS_RULE: NoDeepDestructureInProps =
