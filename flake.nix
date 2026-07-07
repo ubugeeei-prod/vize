15a3341d741947a315ccbd84156717fcbba1245e
Ubugeeei <ubuge1122@gmail.com>
2026-07-07 13:42:56 +0900

    fix(nix): refresh moonbit core hash

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
               };
               coreSrc = pkgs.fetchurl {
                 url = "https://cli.moonbitlang.com/cores/core-latest.tar.gz";
-                hash = "sha256-b8JYDhPvsLfeZq891FMoQYclpQhduaRF1Y1V/nk8TUU=";
+                hash = "sha256-PHPWerElUb0jNNM4nq99AzbxmsCKucOW8R4SfjdbPQY=";
               };
               nativeBuildInputs = lib.optionals pkgs.stdenv.isLinux [ pkgs.patchelf ];
               dontUnpack = true;
