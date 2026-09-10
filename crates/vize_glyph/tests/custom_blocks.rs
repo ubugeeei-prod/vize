b27ca3aebc528f77cdd2e8e57e943ac84c255281
ubugeeei <ubuge1122@gmail.com>
2026-09-10 16:55:29 +0900

    test(glyph): pin art multiline attribute output

--- crates/vize_glyph/tests/custom_blocks.rs
+++ crates/vize_glyph/tests/custom_blocks.rs
@@ -85,8 +85,22 @@ fn art_block_multiline_attribute_values_are_idempotent() {
     let second = format_sfc(&first.code, &options).unwrap();
     let third = format_sfc(&second.code, &options).unwrap();
 
-    assert!(first.code.contains("\n        height: 100%;\n"));
-    assert!(!first.code.contains("\n          height: 100%;\n"));
+    assert_eq!(
+        first.code.as_str(),
+        r#"<art>
+  <section>
+    <div
+      style="
+        height: 100%;
+        display: flex;
+      "
+    >
+      hello
+    </div>
+  </section>
+</art>
+"#
+    );
     assert_eq!(first.code, second.code);
     assert_eq!(second.code, third.code);
 }
